2585ad730e8a492d53dde71bf426ecfbea36b384
Steven Price <steven.price@arm.com>
2022-05-26 15:01:35 +0100

    arm64: RME: Allow VMM to set RIPAS
    Each page within the protected region of the realm guest can be marked
    as either RAM or EMPTY. Allow the VMM to control this before the guest
    has started and provide the equivalent functions to change this (with
    the guest's approval) at runtime.
    
    When transitioning from RIPAS RAM (1) to RIPAS EMPTY (0) the memory is
    unmapped from the guest and undelegated allowing the memory to be reused
    by the host. When transitioning to RIPAS RAM the actual population of
    the leaf RTTs is done later on stage 2 fault, however it may be
    necessary to allocate additional RTTs to allow the RMM track the RIPAS
    for the requested range.
    
    When freeing a block mapping it is necessary to temporarily unfold the
    RTT which requires delegating an extra page to the RMM, this page can
    then be recovered once the contents of the block mapping have been
    freed. A spare, delegated page (spare_page) is used for this purpose.
    
    Signed-off-by: Steven Price <steven.price@arm.com>

--- arch/arm64/kvm/mmu.c
+++ arch/arm64/kvm/mmu.c
@@ -382,6 +382,7 @@ static int pkvm_unmap_range(struct kvm *kvm, u64 start, u64 end)
  * @start: The intermediate physical base address of the range to unmap
  * @size:  The size of the area to unmap
  * @may_block: Whether or not we are permitted to block
+ * @only_shared: If true then protected mappings should not be unmapped
  *
  * Clear a range of stage-2 mappings, lowering the various ref-counts.  Must
  * be called while holding mmu_lock (unless for freeing the stage2 pgd before
@@ -398,7 +399,7 @@ static int ___unmap_stage2_range(struct kvm_s2_mmu *mmu, u64 addr, u64 size)
 }
 
 static void __unmap_stage2_range(struct kvm_s2_mmu *mmu, phys_addr_t start, u64 size,
-				 bool may_block)
+				 bool may_block, bool only_shared)
 {
 	struct kvm *kvm = kvm_s2_mmu_to_kvm(mmu);
 	phys_addr_t end = start + size;
@@ -415,7 +416,7 @@ static void __unmap_stage2_range(struct kvm_s2_mmu *mmu, phys_addr_t start, u64 
 void kvm_stage2_unmap_range(struct kvm_s2_mmu *mmu, phys_addr_t start,
 			    u64 size, bool may_block)
 {
-	__unmap_stage2_range(mmu, start, size, may_block);
+	__unmap_stage2_range(mmu, start, size, may_block, false);
 }
 
 static void pkvm_stage2_flush(struct kvm *kvm)
@@ -2347,7 +2348,8 @@ bool kvm_unmap_gfn_range(struct kvm *kvm, struct kvm_gfn_range *range)
 
 	__unmap_stage2_range(&kvm->arch.mmu, range->start << PAGE_SHIFT,
 			     (range->end - range->start) << PAGE_SHIFT,
-			     range->may_block);
+			     range->may_block,
+			     range->only_shared);
 
 	kvm_nested_s2_unmap(kvm, range->may_block);
 	return false;
